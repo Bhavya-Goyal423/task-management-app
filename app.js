86a227ad0a12e79439d4a4c2e0046a92310f4ec9
Bhavya-Goyal423 <goel2000bhavya@gmail.com>
2024-11-23 00:40:17 +0530

    added global error handler

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorhandler = require('./controller/errorController');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/user', userRouter);
+
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'failed',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+app.use(globalErrorhandler);
 
 module.exports = app;
